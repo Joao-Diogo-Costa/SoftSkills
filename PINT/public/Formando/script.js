f591577dc9265391e5248c9900f3e8e5cf950b2c
João Costa <pv25169@alunos.estgv.ipv.pt>
2025-04-13 21:00:35 +0100

    Add files via upload

--- PINT/public/Formando/script.js
+++ PINT/public/Formando/script.js
@@ -25,3 +25,29 @@ function closeSuccessModal() {
         sugestSucessModal.hide();
     }
 }
+
+// Isto vai servir para apagar conta e mostrar os modais respetivos
+function handleEditProfile() {
+    console.log("A editar informações de perfil...");
+    // Aqui você faria a chamada para sua função real de editar perfil 
+
+    // Após o perfil ser editado com sucesso, mostrar o modal de sucesso
+    const editProfileModalEl = document.getElementById('editProfileModal');
+    const editProfileModal = bootstrap.Modal.getInstance(editProfileModalEl);
+    if (editProfileModal) {
+        editProfileModal.hide();
+    }
+
+    const editProfileSucessModalEl = document.getElementById('editProfileSuccessModal');
+    const editProfileSucessModal = new bootstrap.Modal(editProfileSucessModalEl);
+    editProfileSucessModal.show();
+}
+
+function closeEditProfileSuccessModal() {
+    const editProfileSucessModalEl = document.getElementById('editProfileSuccessModal');
+    const editProfileSucessModal = bootstrap.Modal.getInstance(editProfileSucessModalEl);
+    if (editProfileSucessModal) {
+        editProfileSucessModal.hide();
+    }
+}
+
