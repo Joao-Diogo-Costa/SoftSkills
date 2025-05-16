decde9c8192e79cfb4c7f46696ea1a7e18729887
Carlos Almeida <53129002+krush1x@users.noreply.github.com>
2025-05-16 13:08:33 +0100

    Delete PINT/public/backend/backend directory

--- PINT/public/backend/backend/src/model/database.js
+++
@@ -1,8 +0,0 @@
-var Sequelize = require("sequelize");
-const sequelize = new Sequelize("PINT", "postgres", "111", {
-  host: "localhost",
-  port: "5433",
-  dialect: "postgres",
-  logging: console.log,
-});
-module.exports = sequelize;
--- PINT/public/backend/backend/src/routes/authRoute.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-const router = express.Router();
-
-const authController = require("../controllers/authController");
-
-// POST: Realizar login
-router.post("/login", authController.login);
-
-module.exports = router;
